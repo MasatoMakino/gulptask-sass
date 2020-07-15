a287f66653a2405a8bd53a0555e63e5c7762602b
MasatoMakino <unagiinu.g@gmail.com>
2020-07-15 14:29:35 +0900

    bump to 0.2.1

--- bin/index.js
+++ bin/index.js
@@ -7,7 +7,7 @@ const sass = require("gulp-sass");
 const packageImporter = require("node-sass-package-importer");
 const autoprefixer = require("gulp-autoprefixer");
 const path = require("path");
-const glob = require("glob");
+const globby = require("globby");
 /**
  * sassファイルをcssに変換、出力するgulpタスク。
  * @param entryPoints 変換対象のsass e.g. ["./src/sass/style.sass"]
@@ -29,7 +29,7 @@ function get(entryPoints, destDir) {
 }
 exports.get = get;
 const existsTarget = (entryPoints) => {
-    const targets = glob.sync(entryPoints);
+    const targets = globby.sync(entryPoints);
     if (targets == null || targets.length === 0) {
         console.error("\x1b[31m%s\x1b[0m", `gulptask-sass : Error no target files.
     The file specified by ${entryPoints} does not exist. The SASS conversion task exits without outputting anything.
